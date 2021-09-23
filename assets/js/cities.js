3edb68d000f8c5966407531bb0dcfb148a9295bc
Дмитрий Блещунов <bleschunov@MacBook-Pro-Dmitry.local>
2021-09-24 00:25:26 +0300

    add component dropper

---
+++ assets/js/cities.js
@@ -0,0 +1,54 @@
+const data = {
+"status": "OK",
+"cities":
+  [
+    {
+    "name":"Рязань",
+    "id_ str":"ryazan",
+    "link":"https://prohodka.com? city=ryazan"
+    },
+    {
+      "name":"Москва",
+      "id_ str":"ryazan",
+      "link":"https://prohodka.com? city=ryazan"
+    },
+    {
+      "name":"Владивосток",
+      "id_ str":"ryazan",
+      "link":"https://prohodka.com? city=ryazan"
+    }
+  ]
+}
+
+const template = document.getElementById('item');
+
+let dropper = document.getElementById('cityDropper');
+let currentCityNode = document.getElementById('currentCity');
+let list = document.getElementById('cityMenu');
+
+let currentCity = data.cities[0].name;
+currentCityNode.innerHTML = currentCity;
+
+for (city of data.cities) {
+  let item = template.content.cloneNode(true);
+  item.querySelector(".dropper__frontend").innerHTML = city.name;
+
+  list.appendChild(item);
+
+  let handler = cacheCityName(city.name);
+  list.lastElementChild.addEventListener('click', handler);
+
+  list.lastElementChild.addEventListener('click', () => {
+    dropper.removeAttribute('open');
+  })
+
+
+}
+
+function cacheCityName (cityName) {
+  cityName = city.name;
+
+  return function () {
+    currentCityNode.innerHTML = cityName;
+  }
+}
